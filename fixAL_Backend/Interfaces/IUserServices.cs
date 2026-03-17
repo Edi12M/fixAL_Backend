e90640da29d53322ac84c58ea2d9c9696d7c8cd1
Edvin <edi.mehaj12@gmail.com>
2026-03-17 18:56:47 +0100

    added authentication and user Services Interfaces

---
+++ fixAL_Backend/Interfaces/IUserServices.cs
@@ -0,0 +1,11 @@
+﻿using fixAL_Backend.DTOs.User;
+
+namespace fixAL_Backend.Interfaces;
+
+public interface IUserServices
+{
+    Task<List<UserDto>> GetAllUsers();
+    Task<UserDto> GetUserById(int userId);
+    Task<UserDto> UpdateUser(int id, UpdateUserDto dto);
+    Task<UserDto> DeleteUser(int userId);
+}
